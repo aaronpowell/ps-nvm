288721ae897e4e1b7dab3ae2ee4f3607c2fadfe2
Felix Becker <felix.b@outlook.com>
2017-12-03 04:46:07 -0800

    Trim down README to Getting Started and generate documentation reference (#53)

---
+++ .scripts/Generate-Documentation.ps1
@@ -0,0 +1,71 @@
+
+Import-Module "$PSScriptRoot/../nvm.psd1"
+
+$toc = ''
+$doc = ''
+
+(Get-Module nvm).ExportedCommands.Values.Name | Sort-Object | ForEach-Object {
+    $help = Get-Help $_
+
+    $toc += @"
+- [``$($help.Name)``](#$($help.Name.ToLower()))
+
+"@
+
+    $examples = $help.examples.example | ForEach-Object {
+        @"
+
+``````powershell
+$($_.code)
+``````
+
+$($_.remarks.Text)
+
+"@
+    }
+
+    $parameters = if ($help.parameters.parameter -eq $null) {
+        'None'
+    }
+    else {
+        $help.parameters.parameter | ForEach-Object {
+            @"
+- ``-$($_.name) <$($_.type.name)>``$('  ')
+  $($_.description.Text)
+
+"@
+        }
+    }
+
+    $inlineParameters = ($help.parameters.parameter | Where-Object { $_ -ne $null } | ForEach-Object { "-$($_.name) <$($_.type.name)>" }) -join ' '
+
+    $commandDoc = @"
+### ``$($help.Name)``
+<a id="$($help.Name.ToLower())"></a>
+
+$($help.description.Text)
+
+``````powershell
+$($help.Name) $inlineParameters
+``````
+
+#### Parameters
+$parameters
+
+
+#### Examples
+$examples
+"@
+    $doc += $commandDoc
+}
+
+$fullMd = @"
+<!-- This file is generated. To regenerate, run .scripts/Generate-Documentation.ps1 -->
+
+# Command Reference
+
+$toc
+$doc
+"@
+
+$fullMd | Out-File "$PSScriptRoot/../.docs/reference.md"
