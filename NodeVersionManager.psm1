b040bda5d84f105cb7bc8e8dabf50d5b428c52af
Aaron Powell <me@aaron-powell.com>
2017-12-08 10:51:46 +1100

    removing unused file

--- NodeVersionManager.psm1
+++
@@ -1,7 +0,0 @@
-#requires -version 3.0
-Set-StrictMode -Version Latest
-$ErrorActionPreference = 'Stop'
-
-Write-Warning 'This module file is now just a pass through to importing nvm.psm1'
-
-Import-Module './nvm.psm1'
